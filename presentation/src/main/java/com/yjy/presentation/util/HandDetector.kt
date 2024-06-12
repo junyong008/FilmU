836ececddaf438b14dd04295c7a18ad438cc84fb
junyong <junyong008@gmail.com>
2024-06-12 10:44:26 +0900

    style(HandDetector): 코드 줄바꿈 수정

--- presentation/src/main/java/com/yjy/presentation/util/HandDetector.kt
+++ presentation/src/main/java/com/yjy/presentation/util/HandDetector.kt
@@ -59,24 +59,52 @@ class HandDetectorImpl @Inject constructor(
     override suspend fun getExtendedFingerCount(bitmap: Bitmap): Int = withContext(defaultDispatcher) {
         val landmarks = detectInImage(bitmap)?.landmarks() ?: return@withContext 0
         var count = 0
+
         for (landmark in landmarks) {
             if (landmark[HandLandmark.WRIST].y() < landmark[HandLandmark.PINKY_DIP].y()) continue
-            if (isFingerExtended(landmark[HandLandmark.INDEX_FINGER_TIP], landmark[HandLandmark.INDEX_FINGER_PIP], landmark[HandLandmark.WRIST])) count++
-            if (isFingerExtended(landmark[HandLandmark.MIDDLE_FINGER_TIP], landmark[HandLandmark.MIDDLE_FINGER_PIP], landmark[HandLandmark.WRIST])) count++
-            if (isFingerExtended(landmark[HandLandmark.RING_FINGER_TIP], landmark[HandLandmark.RING_FINGER_PIP], landmark[HandLandmark.WRIST])) count++
-            if (isFingerExtended(landmark[HandLandmark.PINKY_TIP], landmark[HandLandmark.PINKY_PIP], landmark[HandLandmark.WRIST])) count++
-            if (isThumbExtended(landmark[HandLandmark.THUMB_TIP], landmark[HandLandmark.THUMB_IP], landmark[HandLandmark.THUMB_CMC], landmark[HandLandmark.INDEX_FINGER_MCP], landmark[HandLandmark.PINKY_MCP])) count++
+
+            val fingers = listOf(
+                HandLandmark.INDEX_FINGER_TIP to HandLandmark.INDEX_FINGER_PIP,
+                HandLandmark.MIDDLE_FINGER_TIP to HandLandmark.MIDDLE_FINGER_PIP,
+                HandLandmark.RING_FINGER_TIP to HandLandmark.RING_FINGER_PIP,
+                HandLandmark.PINKY_TIP to HandLandmark.PINKY_PIP
+            )
+
+            for ((tip, pip) in fingers) {
+                if (isFingerExtended(landmark[tip], landmark[pip], landmark[HandLandmark.WRIST])) count++
+            }
+
+            if (isThumbExtended(
+                    landmark[HandLandmark.THUMB_TIP],
+                    landmark[HandLandmark.THUMB_IP],
+                    landmark[HandLandmark.THUMB_CMC],
+                    landmark[HandLandmark.INDEX_FINGER_MCP],
+                    landmark[HandLandmark.PINKY_MCP]
+                )
+            ) {
+                count++
+            }
         }
         return@withContext count
     }
 
-    private fun isFingerExtended(tip: NormalizedLandmark, pip: NormalizedLandmark, wrist: NormalizedLandmark): Boolean {
+    private fun isFingerExtended(
+        tip: NormalizedLandmark,
+        pip: NormalizedLandmark,
+        wrist: NormalizedLandmark
+    ): Boolean {
         val tipToWrist = calculateDistance(tip, wrist)
         val pipToWrist = calculateDistance(pip, wrist)
         return tipToWrist > pipToWrist
     }
 
-    private fun isThumbExtended(tip: NormalizedLandmark, ip: NormalizedLandmark, cmc: NormalizedLandmark, idxMcp: NormalizedLandmark, pkyMcp: NormalizedLandmark): Boolean {
+    private fun isThumbExtended(
+        tip: NormalizedLandmark,
+        ip: NormalizedLandmark,
+        cmc: NormalizedLandmark,
+        idxMcp: NormalizedLandmark,
+        pkyMcp: NormalizedLandmark
+    ): Boolean {
         val tipToIdxMcp = calculateDistance(tip, idxMcp)
         val tipToPkyMcp = calculateDistance(tip, pkyMcp)
         return tipToIdxMcp < tipToPkyMcp && isStraightAngle(tip, ip, cmc)
@@ -88,12 +116,20 @@ class HandDetectorImpl @Inject constructor(
         return sqrt((xDiff * xDiff + yDiff * yDiff).toDouble())
     }
 
-    private fun isStraightAngle(A: NormalizedLandmark, B: NormalizedLandmark, C: NormalizedLandmark): Boolean {
+    private fun isStraightAngle(
+        A: NormalizedLandmark,
+        B: NormalizedLandmark,
+        C: NormalizedLandmark
+    ): Boolean {
         val angle = calculateAngle(A, B, C)
         return angle > 140 && angle < 220
     }
 
-    private fun calculateAngle(A: NormalizedLandmark, B: NormalizedLandmark, C: NormalizedLandmark): Double {
+    private fun calculateAngle(
+        A: NormalizedLandmark,
+        B: NormalizedLandmark,
+        C: NormalizedLandmark
+    ): Double {
         val ABx = B.x() - A.x()
         val ABy = B.y() - A.y()
         val CBx = B.x() - C.x()
